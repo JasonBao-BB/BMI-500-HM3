de39377d12c695880dc4fbb61e1ca4cd163e9d65
JasonBao-BB <371700894@qq.com>
2020-09-06 23:55:56 -0400

    Add Results Images

--- mods/evaluation.py
+++ mods/evaluation.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 
 def evaluate(labels, seeds_category):
-    df = pd.DataFrame({'labels': labels, 'varieties': seeds_category})
+    df = pd.DataFrame({'labels': labels, 'category': seeds_category})
     # print(df)
     # Create cross-tabulation assigned to ct
-    ct = pd.crosstab(df['labels'], df['varieties'])
+    ct = pd.crosstab(df['labels'], df['category'])
     # Display cross-tabulation
     # The cross-tabulation shows that the 3 varieties of grain separate really well into 3 clusters
     print(ct)
